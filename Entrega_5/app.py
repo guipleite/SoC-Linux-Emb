3cabb87f25574c399bb5662f256fba2627f09e91
labarqcomp <you@example.com>
2020-11-06 00:31:02 -0300

    reading states

--- Entrega_5/app.py
+++ Entrega_5/app.py
@@ -1,9 +1,41 @@
 # export FLASK_APP=/work/app.py
 # python3 -m flask run -h 169.254.0.13
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return render_template("form.html")
+
+led_on = True
+
+@app.route('/toggle_led', methods = ['GET', 'POST'])
+def toggle_led():
+    if request.method == 'POST':
+        global led_on
+        
+        if led_on :
+            led_on = False
+        
+        else:
+            led_on = True
+
+        print("LED State: ",led_on)
+
+    return render_template("form.html")
+
+@app.route('/check_sw', methods = ['GET', 'POST'])
+def check_sw():
+    if request.method == 'POST':
+        # global sw_on
+        
+        if sw_on :
+            sw_on = False
+        
+        else:
+            sw_on = True
+
+        print("Switch State: ",sw_on)
+
     return render_template("form.html")
